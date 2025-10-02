e1e127510fe40c03c5f01ac19be763172740d598
Aryan dutt Gaur <arushsharma2984@gmail.com>
2025-10-02 21:19:38 +0530

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
+echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
+echo "Collecting static files..."
+python manage.py collectstatic --no-input --clear
 
-# Run migrations
-python manage.py migrate
+echo "Running migrations..."
+python manage.py migrate --no-input
+
+echo "Build completed successfully!"
